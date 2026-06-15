900d5d18219ab0f0899c48e81ba6fe358dfc8c59
Ramesha-Dasangi <rameshadasangi4@gmail.com>
2026-06-15 19:56:40 +0530

    Added Splash Screen UI and Login Activity hero header

---
+++ app/src/main/java/com/nibm/pharmagomadproject/MainActivity.java
@@ -0,0 +1,43 @@
+package com.nibm.pharmagomadproject;
+
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.os.Bundle;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+import com.nibm.pharmagomadproject.customer.HomeActivity;
+import com.nibm.pharmagomadproject.customer.LoginActivity;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_main);
+
+        getSupportActionBar().hide();
+
+        SharedPreferences prefs = getSharedPreferences("pharmago_prefs", MODE_PRIVATE);
+        boolean isLoggedIn = prefs.getBoolean("is_logged_in", false);
+
+        Intent intent;
+        if (isLoggedIn)
+        {
+            intent = new Intent(MainActivity.this, HomeActivity.class);
+        } else
+        {
+            intent = new Intent(MainActivity.this, LoginActivity.class);
+        }
+
+        startActivity(intent);
+        finish();
+
+
+    }
+}
